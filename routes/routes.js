57caf59e442969eb31ac238129191e16b8e3c05f
Anirudha5997 <dudhasagare.a@northeastern.edu>
2024-02-07 20:15:11 -0500

    Assignment 2

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const { getHealth } = require("../controllers/health");
-// const {setReqHeaders} = require('../middlewares/setHealthHeader')
+const { registerUser, getUser, updateUser } = require('../controllers/userControllers');
+const verifyToken = require('../middlewares/userAuth');
+
 
 const appRouter = express.Router();
 
 //check service instance health
 appRouter.all("/healthz", getHealth);
+
+appRouter.post("/v1/user", registerUser);
+appRouter.get("/v1/user/self", verifyToken , getUser);
+appRouter.put("/v1/user/self", verifyToken, updateUser);
 
 module.exports = appRouter;
--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const cors = require('cors');
 const appRouter = require("./routes/routes");
+const {sequelize, initialize} = require("./config/connection")
+const User = require("./models/userModel")
+
 require('dotenv').config()
 const PORT = 8000;
 
 const app = express();
 app.use(cors());
 //middlewares
-
 app.use(express.json()); // returns json body object
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-});
+initialize()
+    .then(_ => {
+        sequelize.sync({alter: true})
+    .then((result) => {
+        console.log(result.models)    
+    })
+    .catch((err) => console.log(err));
+    })
 
-app.use("/api/v1", appRouter);
+    .catch(e => {
+        console.log("Initialize Error", e)
+    });
+
+app.listen(PORT, () => console.log("app is running on port 8000"));
+
+app.use("", appRouter);
