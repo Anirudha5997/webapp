67a7acc737c787ecec37e3962a399ee125979ee6
Anirudha5997 <144744094+Anirudha5997@users.noreply.github.com>
2024-03-18 00:21:12 -0400

    Assign61 (#30)
    * packer image update
    
    * packer fmt
    
    * fixed sym link

--- packer/ops_Agent_vm_config.sh
+++ packer/ops_Agent_vm_config.sh
@@ -4,5 +4,7 @@ sudo mv -f /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 sudo cat /etc/google-cloud-ops-agent/config.yaml
 cd /var/log/ || exit
 sudo mkdir webappLogs
+cd /home/prodApp/ || exit
+sudo touch webapp.log
 ln -s /home/prodApp/webapp.log /var/log/webapp/webapp.log
 sudo systemctl restart google-cloud-ops-agent
